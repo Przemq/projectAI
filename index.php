db396c3a35b67b4442c406827f1e142cd845c81b
Przemek <przemek.kudla@nurtureagency.com>
2017-10-07 14:25:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Project</title>
+    <meta name="Internet Applications Project" content="Internet Applications Project">
+    <meta name="author" content="Przemysław Kudła">
+
+    <link rel="stylesheet" href="css/global-style.css">
+    <link rel="stylesheet" href="js/libs/bootstrap/css/bootstrap.min.css">
+
+
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-sm-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deleniti est qui.</div>
+    </div>
+</div>
+<script src="js/scripts/jquery-3.2.1.min.js"></script>
+<script src="js/scripts/global-scripts.js"></script>
+</body>
+</html>
